d868d26c58bfaae66a86fe8360f8dd8c99485a22
Justin Singh <justinsingh2015@gmail.com>
2019-04-11 18:33:48 -0700

    Fixed error where certain track names included an incorrect quotation mark. Made get_new_music in music_tracker.py crawl over a specified range of new music pages rather than just the first page.

--- music_tracker.py
+++ music_tracker.py
@@ -29,36 +29,39 @@ def get_new_music():
     :return: (dictionary) key = music artist, value = newly released
             track(s) (list of strings)
     """
-    # web page we are using to find newly released music
-    new_music_page = requests.get(BASE_URL + "/reviews/tracks/?page=2")
-
-    soup = BeautifulSoup(new_music_page.text, 'html.parser')
-    artist_list_html = soup.find_all(class_ = 'artist-list')
-    track_title_html = soup.find_all(class_ = 'track-collection-item__title')
-
-    # music dictionary that will map list
+    # music dictionary that maps artist names to new music titles
     music_dict = {}
 
-    # iterate over each track and assign values to music_dict
-    for artist in artist_list_html:
-        # get list of artists involved in current track
-        artists = artist.find_all('li')
-        current_artist_list = [artist.get_text() for artist in artists]
+    for i in range(1, 4):
+        # web page we are using to find newly released music
+        new_music_page = requests.get(BASE_URL + "/reviews/tracks/?page=" +
+                                      str(i))
 
-        # get name of current track
-        track_name = artist.find_next_sibling('h2').get_text().strip('“”')
+        soup = BeautifulSoup(new_music_page.text, 'html.parser')
+        artist_list_html = soup.find_all(class_ = 'artist-list')
+        track_title_html = soup.find_all(class_ = 'track-collection-item__title')
 
-        # iterate over artists in the list of artists who made the track
-        # if artist is already in music_dict, append to the list at
-        # music_dict[artist_name]
-        for artist_name in current_artist_list:
-            if artist_name in music_dict:
-                music_dict[artist_name].append(track_name)
-            else:
-                music_dict[artist_name] = [track_name]
+        # iterate over each track and assign values to music_dict
+        for artist in artist_list_html:
+            # get list of artists involved in current track
+            artists = artist.find_all('li')
+            current_artist_list = [artist.get_text() for artist in artists]
+
+            # get name of current track
+            track_name = artist.find_next_sibling('h2').get_text()
+            track_name = track_name.replace('“', '')
+            track_name = track_name.replace('”', '')
+
+            # iterate over artists in the list of artists who made the track
+            # if artist is already in music_dict, append to the list at
+            # music_dict[artist_name]
+            for artist_name in current_artist_list:
+                if artist_name in music_dict:
+                    music_dict[artist_name].append(track_name)
+                else:
+                    music_dict[artist_name] = [track_name]
 
     return music_dict
-
 
 def main():
     # setup csv file with top row headings: Music Artist, Twitter Volume
